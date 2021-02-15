ca19ad1929c511b6bb4923fdc15fd83271d25d20
thibaultcrn1 <tcarron6@outlook.com>
2021-02-15 21:51:13 +0100

    Update index.py (Add go home system)

--- index.py
+++ index.py
@@ -58,31 +58,85 @@ def condensateur():
         Clear()
 
         print("1 - Capacité d'un condensateur")
+        print("\nFormule : \nLorem ipsum dolor inpum lorem plor.\n\n")
+        esc1 = float(input("1 - Calculer, 2 - Retourner à l'Accueil."))
+
+        if esc1 == 1:
+            print("TODO MAKE CALCULATOR SYSTEM")
+        if esc1 == 2:
+            condensateur()
+        else:
+            return;
 
     if condresult == 2:
         Clear()
 
         print("2 - Charge emmagasinée en Coulomb")
+        print("\nFormule : \nLorem ipsum dolor inpum lorem plor.\n\n")
+        esc2 = float(input("1 - Calculer, 2 - Retourner à l'Accueil."))
+
+        if esc2 == 1:
+            print("TODO MAKE CALCULATOR SYSTEM")
+        if esc2 == 2:
+            condensateur()
+        else:
+            return;
 
     if condresult == 3:
         Clear()
 
         print("3 - Energie stockée en joules")
+        print("\nFormule : \nLorem ipsum dolor inpum lorem plor.\n\n")
+        esc3 = float(input("1 - Calculer, 2 - Retourner à l'Accueil."))
+
+        if esc3 == 1:
+            print("TODO MAKE CALCULATOR SYSTEM")
+        if esc3 == 2:
+            condensateur()
+        else:
+            return;
 
     if condresult == 4:
         Clear()
 
         print("4 - Association Parallèle")
+        print("\nFormule : \nLorem ipsum dolor inpum lorem plor.\n\n")
+        esc4 = float(input("1 - Calculer, 2 - Retourner à l'Accueil."))
+
+        if esc4 == 1:
+            print("TODO MAKE CALCULATOR SYSTEM")
+        if esc4 == 2:
+            condensateur()
+        else:
+            return;
 
     if condresult == 5:
         Clear()
 
         print("5 - Association Série")
+        print("\nFormule : \nLorem ipsum dolor inpum lorem plor.\n\n")
+        esc5 = float(input("1 - Calculer, 2 - Retourner à l'Accueil."))
+
+        if esc5 == 1:
+            print("TODO MAKE CALCULATOR SYSTEM")
+        if esc5 == 2:
+            condensateur()
+        else:
+            return;
 
     if condresult == 6:
         Clear()
 
         print("6 - Charge d'un condensateur")
+        print("\nFormule : \nLorem ipsum dolor inpum lorem plor.\n\n")
+        esc6 = float(input("1 - Calculer, 2 - Retourner à l'Accueil."))
+
+        if esc6 == 1:
+            print("TODO MAKE CALCULATOR SYSTEM")
+        if esc6 == 2:
+            condensateur()
+        else:
+            return;
 
     if condresult == 7:
         Clear()
@@ -90,6 +144,13 @@ def condensateur():
         print("7 - Décharge d'un condensateur")
         print("\nFormule : \nLorem ipsum dolor inpum lorem plor.\n\n")
         esc7 = float(input("1 - Calculer, 2 - Retourner à l'Accueil."))
+
+        if esc7 == 1:
+            print("TODO MAKE CALCULATOR SYSTEM")
+        if esc7 == 2:
+            condensateur()
+        else:
+            return;
 
     else:
         return;
